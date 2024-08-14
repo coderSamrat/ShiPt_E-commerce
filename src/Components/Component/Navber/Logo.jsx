57c8c854d0296722e80c1c86620ca50d6b09dce4
coderSamrat <samratmallick832@gmail.com>
2024-08-14 21:34:47 +0530

    new change

--- src/Components/Component/Navber/Logo.jsx
+++ src/Components/Component/Navber/Logo.jsx
@@ -1,12 +1,16 @@
-import { faStudiovinari } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStudiovinari } from '@fortawesome/free-brands-svg-icons';
 
 const Logo = () => {
+      const scrollToTop = () => {
+            window.scrollTo(0, 0);
+      };
+
       return (
             <div className='cursor-pointer font-bold font-Playfair md:text-3xl text-xl tracking-wide'>
-                  <NavLink to='/' className='flex md:gap-2 gap-1' onClick={window.scrollY(0,0)}>
+                  <NavLink to='/' className='flex md:gap-2 gap-1' onClick={scrollToTop} exact>
                         <span className='text-pink-600'>
                               <FontAwesomeIcon icon={faStudiovinari} />
                         </span>
